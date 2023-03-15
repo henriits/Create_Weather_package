b0f98bdd9bc654ff31bfd2d351affe1188b82cdd
henriits <121551949+henriits@users.noreply.github.com>
2023-03-15 22:06:49 +0200

    activateing package

--- main.py
+++
@@ -1,6 +0,0 @@
-"""api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"""
-
-"""b757238a027a9ba7413a3786589d37f2"""
-
-
-"""https://api.openweathermap.org/data/2.5/forecast?q=Madrid&appid=b757238a027a9ba7413a3786589d37f2"""
