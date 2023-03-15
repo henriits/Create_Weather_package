5a4f155bab7179ce259ec93f6f244f0c9cb4c199
henriits <121551949+henriits@users.noreply.github.com>
2023-03-15 20:46:21 +0200

    adding docstrings for class

--- sunnyday.py
+++ sunnyday.py
@@ -2,6 +2,30 @@ import requests
 
 
 class Weather:
+    """
+    Creates a weather object getting an api key as input
+    and either a city name or lat and lon coordinates.
+
+    package use example:
+
+    # Create a weather object using a city name:
+    # The api key below is not guaranteed to work.
+    # Get your own api key from https://openweathermap.org
+    # And wait a couple of hours for api key to be activated.
+
+    > weather = Weather(apikey="b757238a027a9ba7413a3786589d37f2", city="Tallinn")
+
+    # Using latitude and longitude coordinates
+    > weather = Weather(apikey="b757238a027a9ba7413a3786589d37f2", lat=5.3, lon=42.2)
+
+    # Get complete weather data for the next 12 hours:
+    > weather.next_12h()
+
+    # Simplified data for the next 12 hours:
+    > weather1.next12h_simplified()
+
+    """
+
     def __init__(self, city=None, lat=None, lon=None, apikey=None):
         if not apikey:
             raise ValueError("API key not provided")
@@ -42,5 +66,5 @@ class Weather:
         return simple_data
 
 
-weather = Weather(apikey="b757238a027a9ba7413a3786589d37f2", city="Twadawddnn")
+weather = Weather(apikey="b757238a027a9ba7413a3786589d37f2", lat=5.3, lon=42.2)
 print(weather.next_12h_simplified())
